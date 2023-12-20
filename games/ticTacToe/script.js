0f806796c57f91401e727489f6ed5cc1b40d7ac2
Jonas Belak <jonas.belak05@gmail.com>
2023-12-20 10:09:22 +0100

    format code

--- games/ticTacToe/script.js
+++ games/ticTacToe/script.js
@@ -1,7 +1,7 @@
 async function boxClicked(boxIndex) {
     let gameboard = document.querySelector(".game-board")
     let soNichtFreundchen = document.querySelector(".soNichtFreundchen")
-    
+
 
 
     if (gameboard.children[boxIndex].innerHTML != "") {
@@ -13,14 +13,14 @@ async function boxClicked(boxIndex) {
         soNichtFreundchen.classList.add("hidden")
     }
 
-randomPicture(pictureList)
+    randomPicture(pictureList)
 
     setBox(gameboard, boxIndex, "X")
-    
+
     if (didPlayerWin("X") == true) {
         console.log("Checking if X Won")
         return declareWin("X")
-        
+
     }
     console.log(areAllBoxesFilled(), didPlayerWin("X"), didPlayerWin("O"))
     if (areAllBoxesFilled() == true && didPlayerWin("X") == false && didPlayerWin("O") == false) {
@@ -34,13 +34,13 @@ randomPicture(pictureList)
         console.log("Checking if O Won")
         didPlayerWin("O")
     }
-    
+
     //Win
     let XWon = didPlayerWin("X")
     let OWon = didPlayerWin("O")
     let draw = areAllBoxesFilled() == true && didPlayerWin("X") == false && didPlayerWin("O") == false
 
-    
+
 
     if (XWon == true) {
         console.log("You Won")
@@ -64,7 +64,7 @@ function getBox(boxIndex) {
     return gameboard.children[boxIndex].innerHTML
 }
 function bfjPlacement() {
-    let boxIndex =  Math.floor(Math.random()*9)
+    let boxIndex = Math.floor(Math.random() * 9)
     let gameboard = document.querySelector(".game-board")
     if (gameboard.children[boxIndex].innerHTML != "") {
         return bfjPlacement()
@@ -76,10 +76,10 @@ function bfjPlacement() {
 const pictureList = ["../bfjpics/bfj-wins.jpg", "../bfjpics/Big_Pun_1999.jpg", "../bfjpics/big-pun-height-weight-age-body-statistics.jpg", "../bfjpics/Big-Pun.jpg", "../bfjpics/Big-Pun2.jpg", "../bfjpics/big-punisher.jpg", "../bfjpics/iconpic.jpg"]
 
 function randomPicture(pictureList) {
-    let randomPickedPicture = pictureList[Math.floor(Math.random()*6)]
+    let randomPickedPicture = pictureList[Math.floor(Math.random() * 6)]
     let bigFatJoe = document.querySelector(".bigFatJoe")
     console.log(bigFatJoe)
-    bigFatJoe.src=randomPickedPicture
+    bigFatJoe.src = randomPickedPicture
 }
 
 function areAllBoxesFilled() {
@@ -91,7 +91,7 @@ function areAllBoxesFilled() {
     return true;
 }
 function didPlayerWin(playerCharacter) {
-        
+
     //Diagonal
     if (getBox(0) == playerCharacter && getBox(4) == playerCharacter && getBox(8) == playerCharacter) {
         return true
@@ -120,7 +120,7 @@ function didPlayerWin(playerCharacter) {
         return true
     }
     return false
-    
+
 }
 function declareWin(Winner) {
     let heading = document.querySelector(".heading")
@@ -139,33 +139,33 @@ function declareWin(Winner) {
 
 function bfjText(Winner) {
     let bfjText = document.querySelector(".bfjText")
-    bfjWins = [ "HAHA! Niemand kann mich schlagen!",
-                "Ich bin unbesiegbar!",
-                "Big Fat Joe gewinnt mal wieder!",
-                "BOOM! Zerstört hab ich dich!",
-                "So läuft das! Niemand besiegt Big Fat Joe in Tic Tac Toe!",
-                "Du bist keine Herrausforderung. Komm wieder wenn du besser bist!" ]
-    bfjLose = [ "Was?! Das kann nicht sein!",
-                "Du Schummler schummelst!",
-                "Revanche! Ich war noch nicht warmgespielt.",
-                "Wa..Was? Verloren? DAS KANN NICHT SEIN",
-                "Das gibts doch nicht! Du schummelst oder so!",
-                "Denkst auch ich hab ernst gespielt oder? Revanche, und diesmal spiel ich richtig!"]
-    draw    = [ "Nochmal von vorne!",
-                "Boah ey, spiel doch gescheit.",
-                "Ehrlich jetzt? Dann eben nochmal.",
-                "Nochmal.",
-                "So ein Mist, dann eben nochmal von vorne!",
-                "Mach doch mal richtig ._.]" ]
+    bfjWins = ["HAHA! Niemand kann mich schlagen!",
+        "Ich bin unbesiegbar!",
+        "Big Fat Joe gewinnt mal wieder!",
+        "BOOM! Zerstört hab ich dich!",
+        "So läuft das! Niemand besiegt Big Fat Joe in Tic Tac Toe!",
+        "Du bist keine Herrausforderung. Komm wieder wenn du besser bist!"]
+    bfjLose = ["Was?! Das kann nicht sein!",
+        "Du Schummler schummelst!",
+        "Revanche! Ich war noch nicht warmgespielt.",
+        "Wa..Was? Verloren? DAS KANN NICHT SEIN",
+        "Das gibts doch nicht! Du schummelst oder so!",
+        "Denkst auch ich hab ernst gespielt oder? Revanche, und diesmal spiel ich richtig!"]
+    draw = ["Nochmal von vorne!",
+        "Boah ey, spiel doch gescheit.",
+        "Ehrlich jetzt? Dann eben nochmal.",
+        "Nochmal.",
+        "So ein Mist, dann eben nochmal von vorne!",
+        "Mach doch mal richtig ._.]"]
     if (Winner == "O") {
-        bfjText.innerHTML = bfjWins[Math.floor(Math.random()*5)]
+        bfjText.innerHTML = bfjWins[Math.floor(Math.random() * 5)]
         return
     }
     if (Winner == "X") {
-        bfjText.innerHTML = bfjLose[Math.floor(Math.random()*5)]
+        bfjText.innerHTML = bfjLose[Math.floor(Math.random() * 5)]
     }
     else {
-        bfjText.innerHTML = draw[Math.floor(Math.random()*5)]
+        bfjText.innerHTML = draw[Math.floor(Math.random() * 5)]
     }
-    }
+}
 
